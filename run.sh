eda19be276bb07543ae63071218b8d9b905b4bc9
sam <samuel.suhendra@gmail.com>
2020-06-02 12:37:48 +0700

    update

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker rm nsq-exporter
 docker rmi nsq-exporter
 docker build -t nsq-exporter:latest .
-docker run -p 127.0.0.1:9117:9117 --name nsq-exporter nsq-exporter
+docker run -p 127.0.0.1:9117:9117 -d --name nsq-exporter nsq-exporter
